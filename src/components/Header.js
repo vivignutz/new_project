ed03cf0c272dc5657ecad390e60d5e14ed08a366
vivignutz <vgnutzmann@gmail.com>
2023-11-26 17:19:18 +0000

    Modified some style

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,6 @@ function Header() {
     return (
         <nav id="topnav">
             
-{/*   {/* Logo 
-            <div className="nav-logo">
-                <a href="index.html">
-                    <img src={logo} alt="project-logo" className='logo-img' />
-                </a>
-            </div> */}
-
         {/* Navbar links */}
         <div className="nav-links">
             <ul className="navbar-links">
@@ -31,48 +24,6 @@ function Header() {
                 </li>
             </ul>
         </div>
-
-{/* <!-- Search Box (to me implemented): 
-    <div>  
-        <form>
-            <div>
-                    <label> Search Box </label>
-                    <input type="text" placeholder="Search...">
-                <div>
-                    <button type="submit">
-                        <span>
-                            <i> search icon </i>
-                        </span>
-                    </button>
-                </div>
-            </div>    
-        </form>
-    </div>
-
-
-    <div className="user-links">
-        <ul>
-            <li className="user-account">
-                <a href="#" id="user-options"></a>
-                    <div>
-                        <a href="signup">Register</a>
-                        <a href="signin">Login</a>
-                    </div>                            
-            </li>
-
-            <li>
-                <a className="basket" href="basket">
-                    <div>
-                        <div>
-                            <i className="basket icon"></i>
-                        </div>                                 
-                        <p>" €0.00"</p>    
-                    </div>
-                </a>                    
-            </li>
-        </ul>
-    </div>
---> */}
     </nav>                
     );
 }
